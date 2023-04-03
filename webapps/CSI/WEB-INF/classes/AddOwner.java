295a9afb914a0a19a18f6ab7d71d89c6e625180e
Adam Jesse <adam.jesse23@gmail.com>
2023-04-02 22:30:02 -0400

    late night changes

--- webapps/CSI/WEB-INF/classes/AddOwner.java
+++ webapps/CSI/WEB-INF/classes/AddOwner.java
@@ -15,21 +15,20 @@ public class AddOwner extends HttpServlet {
     PrintWriter out = response.getWriter();
 
     String personId = request.getParameter("personId");
-    String name = request.getParameter("name");
     String profession = request.getParameter("profession");
     String income = request.getParameter("income");
 
     try {
-      if (personId.length() == 0 || name.length() == 0) {
-        out.println("Please: Person ID and Name are required");
+      if (personId.length() == 0) {
+        out.println("Please: Person ID is required");
         return;
       }
-      storeOwner(personId, name, profession, income);
+      storeOwner(personId, profession, income);
       out.println("<html><head><title>Owner Registration Report</title>");
       out.print("<br /><b><center><font color=\"RED\"><H2>Owner Registration Report</H2></font>");
       out.println("</center><br />");
 
-      out.println(personId + " " + name + " is now added to the Owners table");
+      out.println(personId + " is now added to the Owners table");
       out.println("</body></html>");
     } catch (Exception ex) {
       out.println("\n Error: " + ex.getMessage());
@@ -48,17 +47,16 @@ public class AddOwner extends HttpServlet {
       String password = "mohammed";
       Connection conn = DriverManager.getConnection(url, user, password);
 
-      pstmt = conn.prepareStatement("insert into owners (PersonId, Name, Profession, Income) values (?, ?, ?, ?)");
+      pstmt = conn.prepareStatement("insert into owners (PersonId, Profession, Income) values (?, ?, ?)");
     } catch (Exception ex) {
       ex.printStackTrace();
     }
   }
 
-  private void storeOwner(String personId, String name, String profession, String income) throws SQLException {
+  private void storeOwner(String personId, String profession, String income) throws SQLException {
     pstmt.setString(1, personId);
-    pstmt.setString(2, name);
-    pstmt.setString(3, profession);
-    pstmt.setString(4, income);
+    pstmt.setString(2, profession);
+    pstmt.setString(3, income);
     pstmt.executeUpdate();
   }
 }
