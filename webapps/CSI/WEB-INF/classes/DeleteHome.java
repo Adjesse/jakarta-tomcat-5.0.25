4905b26896aa8fc1517e99ad8a6dc601cb48d79a
Adam Jesse <adam.jesse23@gmail.com>
2023-04-05 18:27:14 -0400

    working delete home

--- webapps/CSI/WEB-INF/classes/DeleteHome.java
+++ webapps/CSI/WEB-INF/classes/DeleteHome.java
@@ -16,21 +16,14 @@ public class DeleteHome extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         String HOMEID = request.getParameter("HOMEID");
-<<<<<<< HEAD
 		
-=======
->>>>>>> 55fd180facfd71becccb527358efd1ba9bdc24e7
 
         try {
             if (HOMEID.length() == 0) {
                 out.println("Please: Home ID is required");
                 return;
             }
-<<<<<<< HEAD
-			deleteHome(HOMEID);
-=======
             deleteHome(HOMEID);
->>>>>>> 55fd180facfd71becccb527358efd1ba9bdc24e7
             out.println("<html><head><title>Record has deleted</title>");
             out.println("</head><body>");
             out.print("<br /><b><center><font color=\"RED\"><H2>The following record has been deleted from the database:</H2></font>");
@@ -40,10 +33,7 @@ public class DeleteHome extends HttpServlet {
             out.println("</body></html>");
         } catch (Exception ex) {
             out.println("\n Error: " + ex.getMessage());
-<<<<<<< HEAD
     		ex.printStackTrace(); // Add this line to print the stack trace
-=======
->>>>>>> 55fd180facfd71becccb527358efd1ba9bdc24e7
         } finally {
             out.close();
         }
@@ -53,7 +43,6 @@ public class DeleteHome extends HttpServlet {
             throws ServletException, IOException {
         doPost(request, response);
     }
-<<<<<<< HEAD
 
     private void initializeJdbc() {
         try {
@@ -70,29 +59,8 @@ public class DeleteHome extends HttpServlet {
         }
     }
 
-=======
-
-    private void initializeJdbc() {
-        try {
-            String driver = "oracle.jdbc.driver.OracleDriver";
-            Class.forName(driver);
-            String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
-            String user = "CSIPROJECT";
-            String password = "mohammed";
-            Connection conn = DriverManager.getConnection(url, user, password);
-            pstmt = conn.prepareStatement("DELETE FROM homes WHERE HOMEID = ?");
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
->>>>>>> 55fd180facfd71becccb527358efd1ba9bdc24e7
     private void deleteHome(String HOMEID) throws SQLException {
         pstmt.setString(1, HOMEID);
         pstmt.executeUpdate();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 55fd180facfd71becccb527358efd1ba9bdc24e7
