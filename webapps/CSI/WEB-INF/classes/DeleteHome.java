f6714d42571455207d6ec52b5570bd29024c3567
Adam Jesse <adam.jesse23@gmail.com>
2023-04-04 21:54:46 -0400

    tues4 changes

--- webapps/CSI/WEB-INF/classes/DeleteHome.java
+++ webapps/CSI/WEB-INF/classes/DeleteHome.java
@@ -3,97 +3,64 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.sql.*;
 
-public class DeleteHome extends HttpServlet 
-{
+public class DeleteHome extends HttpServlet {
+    private PreparedStatement pstmt;
+
+    public void init() throws ServletException {
+        initializeJdbc();
+    }
+
     public void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException,IOException
-    {        
-			Statement state4 = null;
-			ResultSet result = null;
-			String query="";        
-			Connection con=null; 
-            
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
-                String HOMEID = request.getParameter("HOMEID");
-             
-					
-           
+        String HOMEID = request.getParameter("HOMEID");
+		
 
+        try {
+            if (HOMEID.length() == 0) {
+                out.println("Please: Home ID is required");
+                return;
+            }
+			deleteHome(HOMEID);
+            out.println("<html><head><title>Record has deleted</title>");
+            out.println("</head><body>");
+            out.print("<br /><b><center><font color=\"RED\"><H2>The following record has been deleted from the database:</H2></font>");
+            out.print(HOMEID);
+            out.println("</center><br />");
 
+            out.println("</body></html>");
+        } catch (Exception ex) {
+            out.println("\n Error: " + ex.getMessage());
+    		ex.printStackTrace(); // Add this line to print the stack trace
+        } finally {
+            out.close();
+        }
+    }
 
-		try
-		{			
-            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver()); 
-            con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl", "CSIPROJECT", "mohammed");
-	       	System.out.println("Congratulations! You are connected successfully.");      
-      
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    private void initializeJdbc() {
+        try {
+            String driver = "oracle.jdbc.driver.OracleDriver";
+            Class.forName(driver);
+            String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+            String user = "CSIPROJECT";
+            String password = "mohammed";
+            Connection conn = DriverManager.getConnection(url, user, password);
+            pstmt = conn.prepareStatement("DELETE FROM homes WHERE (HOMEID) = (?)");
+        } catch (Exception ex) {
+            ex.printStackTrace();
 			
-     	}
-        catch(SQLException e)
-		{	
-			System.out.println("Error: "+e);
-			
-			
-			
-		}
-		catch(Exception e) 
-		{
-			System.err.println("Exception while loading  driver");		
-		}
-	    try 
-		{
-        	state4 = con.createStatement();
-		} 
-		catch (SQLException e) 	
-		{
-			System.err.println("SQLException while creating statement");			
-		}
-		
-		response.setContentType("text/html");
-		PrintWriter out = null ;
-		try
-		{
-			out =  response.getWriter();
-		}
-		catch (IOException e) 
-		{
-  			e.printStackTrace();
-		}
-		
-		query = "delete  from  homes where HOMEID  = '"+HOMEID+"'";											
-      
-		
-		out.println("<html><head><title>  Record has deleted</title>");	 
-		out.println("</head><body>");
-		
-		
-		out.print( "<br /><b><center><font color=\"RED\"><H2>The following record has been deleted from the database:</H2></font>");
-		
-        out.print( HOMEID );
-		
-        out.println( "</center><br />" );
-       	try 
-		{ 
-			result=state4.executeQuery(query);
-				
-	  	}
-		catch (SQLException e) 
-		{
-			System.err.println("SQLException while executing SQL Statement."); 
-		}
-		
-		try 
-		{ 
-   			result.close(); 
-			state4.close(); 	
-			con.close();
-    		System.out.println("Connection is closed successfully.");
- 	   	}
-		catch (SQLException e) 
-		{
-			e.printStackTrace();	
-		}
+        }
+    }
 
-  		out.println("</body></html>");
-    } 
+    private void deleteHome(String HOMEID) throws SQLException {
+        pstmt.setString(1, HOMEID);
+        pstmt.executeUpdate();
+    }
 }
